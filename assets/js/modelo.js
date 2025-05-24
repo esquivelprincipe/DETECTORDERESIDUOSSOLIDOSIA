1905e8fa1934f890df514ea379d736e8688d1d88
esquivelprincipe <esquivelexon90@gmail.com>
2025-05-23 19:21:31 -0500

    Update modelo.js

--- assets/js/modelo.js
+++ assets/js/modelo.js
@@ -4,7 +4,7 @@ let video;
 let label = "esperando...";
 // The classifier
 let classifier, img, input;
-let modelURL = 'https://teachablemachine.withgoogle.com/models/iCk_dsWKR/';
+let modelURL = 'https://teachablemachine.withgoogle.com/models/PhVvMIwXp/';
 
 // STEP 1: Load the model!
 function preload() {
